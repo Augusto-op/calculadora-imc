d6fc2408dfbae733df613fad441f5e308e60b6d1
Augusto-op <augustoramossilva444@gmail.com>
2025-05-09 15:04:12 -0300

    criação de calculadora imc
    peso:[float], altura[float]

---
+++ Nova pasta/calculadora-inc.py
@@ -0,0 +1,13 @@
+#1. receber a variavel peso [float]
+#2. receber a variavel de altura[float]
+#3. realizar o resultado do imc
+#4. imprimir o resultadodo imc
+
+peso = float(input("insira o peso:"))
+
+altura = float(input("insira a altura:"))
+resultado = (peso /(altura ** 2))
+print("resultado da divisao é", resultado)
+
+
+
